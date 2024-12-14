23acfc376f9b75c866894019c74a12a25c67edf4
Joery <44531907+Joery-M@users.noreply.github.com>
2024-12-14 14:42:56 +0100

    fix(general): Replace sass with sass-embedded
    Also enabled devSourcemap for css

--- packages/safelight/vite.config.ts
+++ packages/safelight/vite.config.ts
@@ -43,10 +43,6 @@ export default defineConfig({
         }
     },
     css: {
-        preprocessorOptions: {
-            scss: {
-                api: 'modern'
-            }
-        }
+        devSourcemap: true
     }
 });
