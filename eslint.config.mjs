2026a176dbe2872cff4aaffa9a8cded11fab273e
Joery <44531907+Joery-M@users.noreply.github.com>
2025-01-20 19:52:38 +0100

    feat: :construction_worker: Added lint chore, updated node LTS version

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,7 @@ import vueParser from 'vue-eslint-parser';
 
 export default ts.config(
     {
-        ignores: ['**/dist/**/*', '**/node_modules/**/*']
+        ignores: ['**/dist/**', '**/node_modules/**']
     },
     js.configs.recommended,
     ...ts.configs.recommended,
