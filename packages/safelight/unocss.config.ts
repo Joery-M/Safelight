e8c8e02b98a9b1175edfa40a7d9a69951ecdfef4
Joery <44531907+Joery-M@users.noreply.github.com>
2025-01-27 21:11:46 +0100

    chore(general): :wastebasket: Removed attributify from unocss
    Was messing with other code

--- packages/safelight/unocss.config.ts
+++ packages/safelight/unocss.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig, presetAttributify, presetUno, transformerDirectives } from 'unocss';
+import { defineConfig, presetUno, transformerDirectives } from 'unocss';
 import { presetPrime } from './buildscripts/unocss-preset-primeui';
 
 export default defineConfig({
     outputToCssLayers: true,
     transformers: [transformerDirectives()],
-    presets: [presetUno(), presetAttributify(), presetPrime()]
+    presets: [presetUno(), presetPrime()]
 });
